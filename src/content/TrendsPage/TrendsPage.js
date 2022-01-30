d30f072dba18645167e216c98af4fc7182c876cf
Matthew Rothera <mjrothera@outlook.com>
2022-01-30 14:04:28 -0500

    Added selectable drop-down for measure

--- src/content/TrendsPage/TrendsPage.js
+++ src/content/TrendsPage/TrendsPage.js
@@ -2,6 +2,13 @@ import React from 'react';
 import { Component } from 'react';
 
 import TrendChart from '../../components/TrendChart';
+import Select from 'react-select'
+const options = [
+  { value: 'watertemp', label: 'Water Temperature (F)' },
+  { value: 'temp', label: 'Outside Temperature (F)' },
+  { value: 'wind', label: 'Wind Speed (mph)' },
+  { value: 'waterlight', label: 'Water Light (lumens)' }
+]
 
 class TrendsPage extends Component {
 
@@ -11,7 +18,8 @@ class TrendsPage extends Component {
     hints: [],
     yunits: '',
     xtitle: '',
-    ytitle: ''
+    ytitle: '',
+    selectedMeasure: 'watertemp'
   };
   constructor(props) {
     super(props);
@@ -20,7 +28,8 @@ class TrendsPage extends Component {
       hints: [],
       yunits: '',
       xtitle: '',
-      ytitle: ''
+      ytitle: '',
+      selectedMeasure: 'watertemp'
     };
   }
 
@@ -28,7 +37,7 @@ class TrendsPage extends Component {
     const headers = { 'x-api-key': process.env.REACT_APP_OYSTER_HAVEN_QUERY };
     try {
       var url = new URL("https://ed4b9t53ua.execute-api.us-east-1.amazonaws.com/prod/historical"),
-        params = { measure: "watertemp", timeframe: 1 }
+        params = { measure: this.state.selectedMeasure, timeframe: 1 }
       Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
       fetch(
         url,
@@ -63,8 +72,20 @@ class TrendsPage extends Component {
 
   }
 
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (prevState.selectedMeasure !== this.state.selectedMeasure) {
+      console.log("New Measure Selected: "+this.state.selectedMeasure)
+      this.getTrends()
+    }
+  }
+
   componentWillUnmount() {
     console.log('Trends Page unmounted');
+  }
+
+  handleMeasureChange = (selectedOption) => {
+    this.setState({ selectedMeasure: selectedOption.value });
+    //console.log(`Option selected:`, selectedOption.label);
   }
 
   render() {
@@ -74,8 +95,10 @@ class TrendsPage extends Component {
         <div className="bx--row trends-page__banner">
           <div className="bx--col-lg-16">
             <h1 className="trends-page__heading">Latest Trends</h1>
+            <Select options={options} onChange={this.handleMeasureChange} />
           </div>
         </div>
+        
         <div className="bx--row trends-page__r3">
           <TrendChart
             title="Temperature"
